76b5b5757556cb7baa72db5bfb4eb5e7ff1b6293
Shubhendra Prakash <shubhendraprakash1@gmail.com>
2024-10-26 12:01:41 +0530

    initial commit

---
+++ practiceSelenium/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute create Org Test");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modify Org Test");
+	}
+
+}
