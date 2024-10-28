69fbe4eadcf732eea666f42953d7a8469384ec68
Shubhendra Prakash <shubhendraprakash1@gmail.com>
2024-10-28 11:32:50 +0530

    initial commit

---
+++ practiceMaven/src/test/java/pac/contact/contactTest.java
@@ -0,0 +1,22 @@
+package pac.contact;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("execute create Contact");
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println("execute modify Contact");
+	}
+	
+	@Test
+	public void deleteContact() {
+		System.out.println("execute delete Contact");
+	}
+
+}
---
+++ practiceMaven/src/test/java/pac2/orgTest.java
@@ -0,0 +1,21 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createOrg() {
+		System.out.println("execute create Org");
+	}
+	
+	@Test
+	public void modifyOrg() {
+		System.out.println("execute modify Org");
+	}
+	
+	@Test
+	public void deleteOrg() {
+		System.out.println("execute delete Org");
+	}
+
+}
